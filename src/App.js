17e12d4edb92c19dcb2c9c654c078a1e6c9e192c
Tormod Flesjø <tormod.flesjo@gmail.com>
2022-01-01 22:07:49 +0100

    add styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./logo.png";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [rotating, setRotating] = useState(0);
+  const handleStop = () => {};
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-      </header>
-    </div>
+    <main
+      onClick={handleStop}
+      className='App flex justify-center h-screen items-center border-2 '
+    >
+      <img
+        src={logo}
+        className='App-logo animate-spin-slow h-42 lg:h-screen w-auto overflow-hidden'
+        alt='logo'
+      />
+    </main>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 21s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
